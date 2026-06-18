38e3f734921fd948b56db3697f005b9d62763f27
Armin <your-github-email@example.com>
2026-06-18 13:12:53 -0400

    stop tracking .next build artifacts

--- .next/static/chunks/pages/realtors-227fb9317a6d7b16.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[782],{695:(_,n,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/realtors",function(){return u(9762)}])},9762:(_,n,u)=>{"use strict";u.r(n),u.d(n,{__N_SSP:()=>e,default:()=>r});var e=!0;function r(){return null}}},_=>{_.O(0,[636,593,792],()=>_(_.s=695)),_N_E=_.O()}]);
